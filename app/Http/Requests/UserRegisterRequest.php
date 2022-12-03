21fe080c862efc602b9c38926a6c12884b398a65
ahmdriffai <rifai0850@gmail.com>
2022-12-03 16:46:51 +0700

    implement authenticate

---
+++ app/Http/Requests/UserRegisterRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UserRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required'],
+            'email' => ['required', 'unique:users,email'],
+            'password' => ['required'],
+            'role' => ['required'],
+        ];
+    }
+
+    function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+}
